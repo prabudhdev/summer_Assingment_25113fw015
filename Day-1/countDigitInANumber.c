1fbcb85eb621d7bb43357e815bea4551bd720782
PRABUDH TYAGI <prabudhtyagi@gmail.com>
2026-06-13 22:37:41 +0530

    Update countDigitInANumber.c

--- Day-1/countDigitInANumber.c
+++ Day-1/countDigitInANumber.c
@@ -1,16 +1,28 @@
 #include<stdio.h>
+
 int main(){
+
     int n,count=0;
+
     printf("Enter a positive integer:");
+
     scanf("%d",&n);
+
     if(n==0){
         count=1;
     }else{
+
+
         while(n!=0){
+
             n=n/10;
+
             count++;
+
         }
     }
+
     printf("the total digits: %d",count);
+
     return 0;
 }
